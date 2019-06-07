96dfb2e83de3766bf299b0c1484664a1bf2a72a5
Горелкин Александр <s207883@yandex.ru>
2019-06-07 17:00:16 +0300

    Реализована модель сообщения

---
+++ SecurityDoors.WPFApp/Models/Message.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecurityDoors.WPFApp.Models
+{
+	public class Message
+	{
+		public string PersonName { get; set; }
+		public string PersonCard { get; set; }
+		public int DoorId { get; set; }
+		/// <summary>
+		/// TODO: Нужно ли?
+		/// </summary>
+		public int DoorAccessLevel { get; set; }
+	}
+}

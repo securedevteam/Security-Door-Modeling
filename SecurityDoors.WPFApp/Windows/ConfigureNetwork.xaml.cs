0d7f2755675eef8ba5cfd18a9937e55b76e31ddb
Alexey Fedechkin <aleksey-fedechkin@rambler.ru>
2019-06-08 21:51:39 +0700

    косметические изменения

--- SecurityDoors.WPFApp/Windows/ConfigureNetwork.xaml.cs
+++ SecurityDoors.WPFApp/Windows/ConfigureNetwork.xaml.cs
@@ -1,16 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace SecurityDoors.WPFApp.Windows
 {
@@ -22,6 +10,16 @@ namespace SecurityDoors.WPFApp.Windows
         public ConfigureNetwork()
         {
             InitializeComponent();
+        }
+
+        private void Btn_checkNetwork_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Btn_save_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }

c9b98dc1164bdcce92f32ffd8a81ef8fe580104d
Горелкин Александр <s207883@yandex.ru>
2019-06-13 14:26:03 +0300

    Добавлена модель представления DoorsViewModel и интерфейс INotifyPropertyChanged

---
+++ SecurityDoors.WPFApp/Models/ViewModels/DoorsViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecurityDoors.WPFApp.Models.ViewModels
+{
+	public class DoorsViewModel : INotifyPropertyChanged
+	{
+		private string doorName;
+
+		public string DoorName
+		{
+			get => doorName;
+			set
+			{
+				doorName = value;
+				OnPropertyChanged("DoorName");
+			}
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+		public void OnPropertyChanged([CallerMemberName]string prop = "")
+		{
+			if (PropertyChanged != null)
+				PropertyChanged(this, new PropertyChangedEventArgs(prop));
+		}
+	}
+}

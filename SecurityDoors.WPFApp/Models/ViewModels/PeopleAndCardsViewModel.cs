296fa71455f6a3bc72392c415bf450bf7018a556
Горелкин Александр <s207883@yandex.ru>
2019-06-13 14:23:09 +0300

    Добавлена модель представления с интерфейсом INotifyPropertyChanged

---
+++ SecurityDoors.WPFApp/Models/ViewModels/PeopleAndCardsViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecurityDoors.WPFApp.Models.ViewModels
+{
+	public class PeopleAndCardsViewModel : INotifyPropertyChanged
+	{
+		private string name;
+		private string cardNumber;
+
+		public string Name
+		{
+			get => name;
+			set
+			{
+				name = value;
+				OnPropertyChanged("Name");
+			}
+		}
+		public string CardNumber
+		{
+			get => cardNumber;
+			set
+			{
+				name = value;
+				OnPropertyChanged("CardNumber");
+			}
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+		public void OnPropertyChanged([CallerMemberName]string prop = "")
+		{
+			if (PropertyChanged != null)
+				PropertyChanged(this, new PropertyChangedEventArgs(prop));
+		}
+	}
+}

43973c264e17bb90be4d91d7d134ad9484da1cd4
Михаил Масный <masny.mikhail@gmail.com>
2019-07-02 21:01:51 +0300

    Добавлен проект *.UI.Console.

---
+++ SecurityDoors.UI.Console/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecurityDoors.UI.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Самотестирование.
+            // Указать: файл с данными, файл с подключением, повтор: кол-во и период.
+            // Отправка сформированных рандомных значений. Сообхение об этом. Асинхронность.
+        }
+    }
+}

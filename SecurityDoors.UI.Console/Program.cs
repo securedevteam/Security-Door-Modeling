da7fc2ab09f276ad28568bf552fa1bec52f2fbbb
Михаил Масный <masny.mikhail@gmail.com>
2019-07-06 23:41:26 +0300

    Изменено имя и пространство имён проекта *.UI.Console -> *.UI.ConsoleApp.

--- SecurityDoors.UI.Console/Program.cs
+++ SecurityDoors.UI.Console/Program.cs
@@ -1,15 +1,19 @@
-﻿using System;
+﻿using SecurityDoors.BLL.Controllers;
+using SecurityDoors.Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SecurityDoors.UI.Console
+namespace SecurityDoors.UI.ConsoleApp
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.ReadLine();
+
             // Самотестирование.
             // Указать: файл с данными, файл с подключением, повтор: кол-во и период.
             // Отправка сформированных рандомных значений. Сообхение об этом. Асинхронность.

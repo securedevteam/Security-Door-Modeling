7df3b382aba0d195c442b6e7ba565d048c9baf3e
Михаил Масный <masny.mikhail@gmail.com>
2019-07-07 00:49:51 +0300

    Реализована настройка соединения.

--- SecurityDoors.UI.Console/Program.cs
+++ SecurityDoors.UI.Console/Program.cs
@@ -10,13 +10,41 @@ namespace SecurityDoors.UI.ConsoleApp
 {
     class Program
     {
+        private static ConnectionSettings _cs;
+
+
         static void Main(string[] args)
         {
+            ConfigurationSetting();
+            var result = _cs.CheckSettings();
+
+
+
             Console.ReadLine();
 
             // Самотестирование.
             // Указать: файл с данными, файл с подключением, повтор: кол-во и период.
             // Отправка сформированных рандомных значений. Сообхение об этом. Асинхронность.
+        }
+
+
+        private static void ConfigurationSetting()
+        {
+            Console.Write("Enter IP address: ");
+            var ip = Console.ReadLine();
+
+            Console.Write("Enter port: ");
+            var port = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter API port: ");
+            var portAPI = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter secret key: ");
+            var key = Console.ReadLine();
+
+            _cs = new ConnectionSettings(ip, port, portAPI, key);
+
+
         }
     }
 }

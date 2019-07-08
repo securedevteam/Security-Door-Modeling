cb7df57d974c34aa254a9087514bb5682af0ff2c
Горелкин Александр <s207883@yandex.ru>
2019-07-08 18:16:34 +0300

    Реализована работа лога в MainWindow.

--- SecurityDoors.UI.WPF/View/MainWindow.xaml.cs
+++ SecurityDoors.UI.WPF/View/MainWindow.xaml.cs
@@ -1,10 +1,9 @@
 ﻿using SecurityDoors.BLL.Controllers;
-using SecurityDoors.BLL.Properties;
-using SecurityDoors.DAL.Models;
-using SecurityDoors.DAL.ViewModels;
-using System.Collections.Generic;
+using SecurityDoors.Core;
+using System;
 using System.Text.RegularExpressions;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace SecurityDoors.UI.WPF.View
 {
@@ -12,15 +11,25 @@ namespace SecurityDoors.UI.WPF.View
 	/// Логика взаимодействия для MainWindow.xaml
 	/// </summary>
 	public partial class MainWindow : Window
-	{		
-		//TODO: Исправить именование класса на более понятное
-		private DataGridViewModel dataGridView = new DataGridViewModel();
-		private DoorViewModel doorsViewModel = new DoorViewModel();
-        private ConnectionSettings _cs = new ConnectionSettings();
+	{
+		private DispatcherTimer timer;
+		private ConnectionSettings _cs = new ConnectionSettings();
 		public MainWindow()
 		{
 			InitializeComponent();
-			InitializeData();
+			InterfaceUpdate();
+
+			timer = new DispatcherTimer() { Interval = new TimeSpan(0, 0, 1) };
+			timer.Tick += InterfaceUpdate;
+			timer.Start();
+		}
+
+		/// <summary>
+		/// Метод для обновления данных на форме
+		/// </summary>
+		private void InterfaceUpdate(object sender = null, object e = null)
+		{
+			textBox_log.Text = Logger.Log != null ? Logger.Log : string.Empty;
 		}
 
 		/// <summary>
@@ -28,80 +37,14 @@ namespace SecurityDoors.UI.WPF.View
 		/// </summary>
 		private void InitializeData()
 		{
-			dataGrid_persons.ItemsSource = dataGridView.PeopleAndCardsList;
-			comboBox_door.ItemsSource = doorsViewModel.Doors;
 
-
-			//var isServerAvailable = TCPController.CheckServerAvailability();
-			//if (isServerAvailable)
-			//{
-			//	var listOfCards = TCPController.GetListOfStringCardsFromAPI();
-			//	var listOfDoors = TCPController.GetListOfDoorsFromAPI();
-			//	doorsViewModel.Doors.AddRange(listOfDoors);
-
-			//	///Так как было решено ограничиться только передачей карт и дверей, для наглядности люди будут генерироваться случайным образом, с присвоением им реальных карт.
-			//	//var rnd = new DataRandomiserController();
-			//	//rnd.MakeRandomData();
-			//	//var listOfPeole = rnd.randomPeople;
-
-			//	//int i = 0;
-			//	//foreach (var person in listOfPeole)
-			//	//{
-			//	//	person.CardUniqueNumber = listOfCards[i];
-			//	//	var newPerson = new Person()
-			//	//	{
-			//	//		CardUniqueNumber = person.CardUniqueNumber,
-			//	//		FirstName = person.FirstName,
-			//	//		SecondName = person.SecondName,
-			//	//		LastName = person.LastName
-			//	//	};
-			//	//	dataGridView.PeopleAndCardsList.Add(newPerson);
-			//	//	if (i >= listOfCards.Count + 1)
-			//	//	{
-			//	//		i = 0;
-			//	//	}
-			//	//	else
-			//	//	{
-			//	//		i++;
-			//	//	}
-			//	//}
-
-			//	//Записать скачанное в кэш
-			//	//var cacheController = new CacheController
-			//	//{
-			//	//	Doors = listOfDoors,
-			//	//	//People = listOfPeole
-			//	//};
-			//	//cacheController.SaveCacheData();
-			//}
-			//else
-			//{
-			//	//var cacheController = new CacheController();
-			//	//cacheController.LoadCacheData();
-			//	//foreach (var person in cacheController.People)
-			//	//{
-			//	//	dataGridView.PeopleAndCardsList.Add(new Person() { CardUniqueNumber = person.CardUniqueNumber, FirstName = person.FirstName, SecondName = person.SecondName, LastName = person.LastName });
-			//	//}
-			//	//foreach (var door in cacheController.Doors)
-			//	//{
-			//	//	doorsViewModel.Doors.Add(new Door() { Name = door.Name });
-			//	//}
-			//}
 		}
 		/// <summary>
 		/// обработчик кнопки "запустить"
 		/// </summary>
 		private void Btn_run_Click(object sender, RoutedEventArgs e)
 		{
-			var listofMessages = new List<TCPMessage>();
-			foreach (var row in dataGridView.PeopleAndCardsList)
-			{
-				if (row.Use && comboBox_door.SelectedValue != null)
-				{
-					listofMessages.Add(new TCPMessage() {PersonCard = row.CardUniqueNumber, DoorName = comboBox_door.SelectedValue.ToString()});
-				}
-			}
-			//TCPController.SendMessages(listofMessages);
+
 		}
 
 		/// <summary>
@@ -109,34 +52,16 @@ namespace SecurityDoors.UI.WPF.View
 		/// </summary>
 		private void Btn_configureNetwork_Click(object sender, RoutedEventArgs e)
 		{
+			Logger.Log = Constants.SETTING_OPENING_WINDOW;
 			ConfigureNetwork configureNetwork = new ConfigureNetwork(_cs);
 			configureNetwork.ShowDialog();
 		}
 
 		/// <summary>
-		/// обработчик кнопки "добавить действие"
-		/// происходит добавление всех сотрудников отмеченых для проведения тестирования 
-		/// в <c>Action</c> и добавление этого действия в колекцию действий
-		/// </summary
-		private void Btn_addInTest_Click(object sender, RoutedEventArgs e)
-		{
-
-		}
-
-		/// <summary>
-		/// проверка ввода в полне <c>field_pause</c> на то что введены только цифры и точка
+		/// проверка ввода в полне <c>field_pause</c> и <c>field_repeat</c> на то что введены только цифры
 		/// иначе ввод игнорируется 
 		/// </summary>
-		private void TextBox_pause_PreviewTextInput(object sender, System.Windows.Input.TextCompositionEventArgs e)
-		{
-			e.Handled = new Regex("[^0-9.]+").IsMatch(e.Text);
-		}
-
-		/// <summary>
-		/// проверка ввода в полне <c>field_repeat</c> на то что введены только цифры
-		/// иначе ввод игнорируется 
-		/// </summary>
-		private void TextBox_repeat_PreviewTextInput(object sender, System.Windows.Input.TextCompositionEventArgs e)
+		private void TextBox_PreviewTextInput(object sender, System.Windows.Input.TextCompositionEventArgs e)
 		{
 			e.Handled = new Regex("[^0-9]+").IsMatch(e.Text);
 		}

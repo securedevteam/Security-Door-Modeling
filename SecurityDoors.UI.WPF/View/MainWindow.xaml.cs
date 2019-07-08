4b16e6da6bf959650b7026501ce14366ba6ac591
Горелкин Александр <s207883@yandex.ru>
2019-07-08 19:08:03 +0300

    Реализовано: полужение данных из API, кеширование данных. Заполнение combobox_doors данными.

--- SecurityDoors.UI.WPF/View/MainWindow.xaml.cs
+++ SecurityDoors.UI.WPF/View/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using SecurityDoors.BLL.Controllers;
 using SecurityDoors.Core;
+using SecurityDoors.DAL.ViewModels;
 using System;
+using System.Collections.Generic;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Threading;
 
@@ -14,14 +17,21 @@ namespace SecurityDoors.UI.WPF.View
 	{
 		private DispatcherTimer timer;
 		private ConnectionSettings _cs = new ConnectionSettings();
+		private List<string> listOfCards;
+		private List<string> listOfDoors;
+		private CardsUsingViewModel cardsUsingViewModel;
+
 		public MainWindow()
 		{
 			InitializeComponent();
-			InterfaceUpdate();
 
 			timer = new DispatcherTimer() { Interval = new TimeSpan(0, 0, 1) };
 			timer.Tick += InterfaceUpdate;
 			timer.Start();
+
+			dataGrid_persons.ItemsSource = cardsUsingViewModel;
+
+			LoadDataFromFilesAsync();
 		}
 
 		/// <summary>
@@ -32,13 +42,6 @@ namespace SecurityDoors.UI.WPF.View
 			textBox_log.Text = Logger.Log != null ? Logger.Log : string.Empty;
 		}
 
-		/// <summary>
-		/// Метод для заполнения формы данными с сервера или файла
-		/// </summary>
-		private void InitializeData()
-		{
-
-		}
 		/// <summary>
 		/// обработчик кнопки "запустить"
 		/// </summary>
@@ -55,6 +58,79 @@ namespace SecurityDoors.UI.WPF.View
 			Logger.Log = Constants.SETTING_OPENING_WINDOW;
 			ConfigureNetwork configureNetwork = new ConfigureNetwork(_cs);
 			configureNetwork.ShowDialog();
+		}
+
+		private async void Btn_loadFromApi_Click(object sender, RoutedEventArgs e)
+		{
+			Logger.Log = Constants.DATA_API_DOWNLOADING_STARTED;
+
+			try
+			{
+				var dataOperation = new DataOperations(_cs);
+				var result = await dataOperation.DownloadDataFromAPIAsync();
+
+				if (result)
+				{
+					await LoadDataFromFilesAsync();
+
+					Logger.Log = Constants.DATA_API_SUCCESSED;
+				}
+				else
+				{
+					Logger.Log = Constants.DATA_API_FAILED;
+				}
+			}
+			catch
+			{
+				Logger.Log = Constants.DATA_API_FAILED;
+			}
+		}
+
+		private async Task LoadDataFromFilesAsync()
+		{
+			Logger.Log = Constants.DATA_READING_STARTED;
+
+			var dataOperation = new DataOperations(_cs);
+			(bool status, List<string> cards, List<string> doors) = await dataOperation.LoadDataAsync();
+
+			if (status)
+			{
+				listOfCards = cards;
+				listOfDoors = doors;
+
+				UpdateDataSource();
+
+				Logger.Log = Constants.DATA_READING_ENDED;
+			}
+			else
+			{
+				comboBox_door.Items.Add(Constants.COMBOBOX_EMPTY);
+				comboBox_door.SelectedItem = Constants.COMBOBOX_EMPTY;
+
+				Logger.Log = Constants.DATA_READING_FAILED;
+			}
+		}
+
+		private void UpdateDataSource()
+		{
+			comboBox_door.Items.Clear();
+			foreach (var door in listOfDoors)
+			{
+				comboBox_door.Items.Add(door);
+			}
+			comboBox_door.SelectedIndex = 0;
+
+			cardsUsingViewModel.Cards.Clear();
+			foreach (var card in listOfCards)
+			{
+				cardsUsingViewModel.Cards.Add(new DAL.Models.Card() { UniqueNumber = card , Use = false });
+			}
+			dataGrid_persons.ItemsSource = cardsUsingViewModel;
+		}
+
+		private async void Btn_SaveToFile_Click(object sender, RoutedEventArgs e)
+		{
+			await LoadDataFromFilesAsync();
 		}
 
 		/// <summary>

c424fb78ba737b8e353628067913c6ed7ad9e6bd
Nishikava <42912978+Nishikava@users.noreply.github.com>
2019-08-09 21:00:25 +0300

    Исправлен проект для тестов с .net Core на .net.

---
+++ SecurityDoorsModelingUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SecurityDoorsModelingUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SecurityDoorsModelingUnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cb2234cc-1ffe-4c47-8e0b-c11abc1b053f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SecurityDoorsModelingUnitTests/ServiceFixture.cs
@@ -0,0 +1,27 @@
+﻿using SecurityDoors.BLL;
+using SecurityDoors.BLL.Controllers;
+using SecurityDoors.BLL.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SecurityDoorModeling.Tests
+{
+    public class ServiceFixture
+    {
+        public ServiceProvider ServiceProvider { get; private set; }
+
+        public ServiceFixture()
+        {
+            var serviceCollection = new ServiceCollection();
+
+            serviceCollection.AddScoped<ICache, Cache>();
+            serviceCollection.AddScoped<IConnectionSettings, ConnectionSettings>();
+            serviceCollection.AddScoped<IDataOperations, DataOperations>();
+            serviceCollection.AddScoped<ITCP, TCP>();
+            serviceCollection.AddScoped<IWebConnection, WebConnection>();
+
+            serviceCollection.AddScoped<DataManager>();
+
+            ServiceProvider = serviceCollection.BuildServiceProvider();
+        }
+    }
+}
